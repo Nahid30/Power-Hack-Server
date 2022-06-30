2c226cd90d39bea7437a737984633bd0e9313daf
Nahid30 <nahidshuvo30@gmail.com>
2022-06-30 21:15:13 +0600

    get data from database and load this datas to the client side

--- index.js
+++ index.js
@@ -1,12 +1,62 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
+const cors = require('cors');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 
-app.get('/', (req, res) =>{
-    res.send("Hello From Power Hack") 
+app.use(cors())
+app.use(express.json());
+
+
+
+
+const uri = "mongodb+srv://PowerHack2:nGkMIUOl3VBcGCW8@cluster0.4l6ze.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   console.log('db connected mamu')
+//   client.close();
+// });
+
+async function run() {
+    try {
+        await client.connect();
+        const billCollection = client.db("powerHack").collection("bill");
+
+        app.get('/bill', async(req, res) => {
+            const query = {};
+            const cursor = billCollection.find(query);
+            const bills = await cursor.toArray();
+            res.send(bills);
+        });
+    }
+    finally {
+        // await client.close();
+    }
+
+}
+run().catch(console.dir);
+
+
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p0sri.mongodb.net/?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send("Hello From Power Hack")
 });
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log('Listening to port', port)
 });
+
+
